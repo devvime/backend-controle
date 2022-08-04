e8bc0f43434277122a060b80ab311a7433749986
Victor Alves <victor.alves@leadforce.com.br>
2022-08-04 19:50:18 -0300

    first commit

--- App/Routes.php
+++ App/Routes.php
@@ -13,15 +13,15 @@ $app->get('/', function($req, $res) {
 
 $app->post('/user', 'UserController@store');
 $app->post('/auth', 'AuthController@auth');
-
-$app->group('/user', function() use($app) {
-    $app->get('', 'UserController@index');
-    $app->get('/:id', 'UserController@find');    
-    $app->put('/:id', 'UserController@update');
-    $app->delete('/:id', 'UserController@destroy');
-}, function() use($authMiddleware) {
-    $authMiddleware->index();
-});
+$app->get('/user', 'UserController@index');
+// $app->group('/user', function() use($app) {
+    
+//     $app->get('/:id', 'UserController@find');    
+//     $app->put('/:id', 'UserController@update');
+//     $app->delete('/:id', 'UserController@destroy');
+// }, function() use($authMiddleware) {
+//     $authMiddleware->index();
+// });
 
 $app->get('/month', 'MonthControler@index', 'AuthMiddleware@index');
 $app->get('/month/:id', 'MonthControler@find', 'AuthMiddleware@index');
