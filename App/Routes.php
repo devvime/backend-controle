03a6eba4ab9ce9e69c70368ab055439c6e17ec29
Victor Alves <victor.alves@leadforce.com.br>
2022-08-06 02:58:28 -0300

    first commit

--- App/Routes.php
+++ App/Routes.php
@@ -10,8 +10,13 @@ $app->get('/', function($req, $res) {
     $res->json(['title'=>'Simple CRUD PHP', 'root'=>$_SERVER['DOCUMENT_ROOT']]);
 });
 
-$app->post('/user', 'UserController@store');
 $app->post('/auth', 'AuthController@auth');
+
+$app->get('/user', 'UserController@index');
+$app->get('/user/:id', 'UserController@find');    
+$app->post('/user', 'UserController@store');
+$app->put('/user/:id', 'UserController@update');
+$app->delete('/user/:id', 'UserController@destroy');
 
 $app->get('/month', 'MonthControler@index', 'AuthMiddleware@index');
 $app->get('/month/:id', 'MonthControler@find', 'AuthMiddleware@index');
@@ -24,14 +29,5 @@ $app->get('/expense/:id', 'ExpenseController@find', 'AuthMiddleware@index');
 $app->post('/expense', 'ExpenseController@store', 'AuthMiddleware@index');
 $app->put('/expense/:id', 'ExpenseController@update', 'AuthMiddleware@index');
 $app->delete('/expense/:id', 'ExpenseController@destroy', 'AuthMiddleware@index');
-
-$app->group('/user', function() use($app) {
-    $app->get('', 'UserController@index');
-    $app->get('/:id', 'UserController@find');    
-    $app->put('/:id', 'UserController@update');
-    $app->delete('/:id', 'UserController@destroy');
-}, function() use($authMiddleware) {
-    $authMiddleware->index();
-});
-
+    
 $app->run();
