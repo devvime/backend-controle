fca1333e14ac7be671e8a795aae8c00b4c9cb29d
Victor Alves <victor.alves@leadforce.com.br>
2022-08-10 17:04:22 -0300

    add user data by token ins month list

--- App/Controllers/MonthController.php
+++ App/Controllers/MonthController.php
@@ -20,7 +20,7 @@ class MonthController extends ControllerService {
 
     public function index($req, $res) {
         $user_token = self::$httpService->getBearerToken();
-        $userData = JWT::encode($req->params->userId, SECRET);
+        $userData = JWT::encode($user_token, SECRET);
         $result = self::$monthModel->select("*");
         $res->json([
             "status"=>200,
